68aa2c5a54779282fc7d3f006e5a730c8ea2c5b6
Goulven Guenan <goulvenguenan@gmail.com>
2024-12-19 10:54:10 +0100

    Update Team.java

--- src/Team.java
+++ src/Team.java
@@ -1,12 +1,14 @@
 import java.util.List;
 
+// Création de la Variable Team
 public class Team {
+    // Initialisation de la liste
     public List<Battler> battlers;
-
+    // Prend en paramètre une liste de Battler  et l'attribut battlers
     public Team(List<Battler> battlers) {
         this.battlers = battlers;
     }
-
+    // Variable qui controle si une equipe a perdu
     public boolean isLoose() {
         for (Battler battler : battlers) {
             if (battler.hp > 0) {
