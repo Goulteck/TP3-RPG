2b1d2a546291aba4bfd7d81e417982e8c3a48d92
Goulven Guenan <goulvenguenan@gmail.com>
2024-12-19 10:54:07 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
 
 public class Main {
+    // Création de combattants
     public static void main(String[] args) {
-        Battler player = new Battler("Marchand", 100, 20);
+        Battler player = new Battler("Marchand", 100, 15);
         Battler enemy = new Battler("Truand", 80, 25);
 
+        // Création des équipes
         Team playerTeam = new Team(Arrays.asList(player));
         Team enemyTeam = new Team(Arrays.asList(enemy));
 
+        // Initialisation et lancement du jeux
         Game game = new Game(playerTeam, enemyTeam);
         game.play();
     }
