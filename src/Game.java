c989d5e006a1d79abf6a21fd9ec944120d28b72b
Goulven Guenan <goulvenguenan@gmail.com>
2024-12-19 10:54:02 +0100

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,9 @@
+// Création de la classe Game qui contient deux attributs
 public class Game {
     public Team playerTeam;
     public Team enemyTeam;
 
+    // initialisation des deux équipes
     public Game(Team playerTeam, Team enemyTeam) {
         this.playerTeam = playerTeam;
         this.enemyTeam = enemyTeam;
@@ -10,9 +12,11 @@ public class Game {
     public void play() {
         System.out.println("Le combat commence !");
 
+        // Initialisation des combattants
         Battler player = playerTeam.battlers.get(0);
         Battler enemy = enemyTeam.battlers.get(0);
 
+        // Boucle de combat
         while (!playerTeam.isLoose() && !enemyTeam.isLoose()) {
             player.attack(enemy); // Le joueur attaque l'ennemi
             if (enemy.hp > 0) {
@@ -20,11 +24,12 @@ public class Game {
             }
         }
 
+        // Détermination du vainqueur
         Team winner = getWinnerTeam();
         System.out.println("L'équipe gagnante est : " +
                 (winner == playerTeam ? "Player Team" : "Enemy Team"));
     }
-
+    // Retourne le vainqueur
     public Team getWinnerTeam() {
         return playerTeam.isLoose() ? enemyTeam : playerTeam;
     }
